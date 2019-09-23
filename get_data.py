a229318b9560f329ab66acc686b76a0078e06bd9
Fischer Jemison <jemisonf@10-249-222-49.wireless.oregonstate.edu>
2019-09-23 11:06:20 -0700

    adds get data script

---
+++ get_data.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import json
+
+if "AUTH_TOKEN" in os.environ:
+    auth_token = os.environ["AUTH_TOKEN"]
+else:
+    raise "No auth token supplied -- use $AUTH_TOKEN environment variable"
+
+headers = {
+    "Authorization": f"{auth_token}",
+    "Content-Type": "application/json"
+}
+
+params = {
+    "page[size]": 10000
+}
+
+r = requests.get("https://api.oregonstate.edu/v1/locations", params, headers=headers)
+
+r.raise_for_status()
+
+data = json.loads(r.text)
+
+locations = data["data"]
+
+corvallis_locations = [location for location in data['data'] if location['attributes']['campus'] == 'Corvallis']
+
+print(f"{100 * len(corvallis_locations) / len(locations)}% of locations are in Corvallis")
