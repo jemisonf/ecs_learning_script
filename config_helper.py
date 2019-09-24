f785ec8b46daef7bd6473d417effab774766c80e
Fischer Jemison <jemisonf@oregonstate.edu>
2019-09-24 10:22:39 -0700

    adds pipenv, changes double quotes to single quotes

--- config_helper.py
+++ config_helper.py
@@ -2,7 +2,7 @@ import configparser
 
 import requests
 
-HOST_DEFAULT = "api.oregonstate.edu"
+HOST_DEFAULT = 'api.oregonstate.edu'
 
 
 class ConfigHelper:
@@ -11,21 +11,21 @@ class ConfigHelper:
         self.config.read(config_file)
 
     def get_access_token(self):
-        if "credentials" not in self.config:
-            raise "config file does not include credentials"
-        credentials = self.config["credentials"]
+        if 'credentials' not in self.config:
+            raise 'config file does not include credentials'
+        credentials = self.config['credentials']
 
         try:
-            client_id = credentials["client_id"]
-            client_secret = credentials["client_secret"]
+            client_id = credentials['client_id']
+            client_secret = credentials['client_secret']
         except KeyError:
-            raise ("credentials section does not contain"
-                   " client_id or client_secret")
+            raise ('credentials section does not contain'
+                   ' client_id or client_secret')
 
         params = {
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "grant_type": "client_credentials",
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials',
         }
         res = requests.post(
             f'{self.get_host_url()}oauth2/token', params)
@@ -33,10 +33,10 @@ class ConfigHelper:
 
         data = res.json()
 
-        if "access_token" in data:
-            return data["access_token"]
+        if 'access_token' in data:
+            return data['access_token']
         else:
-            raise "returned JSON data does not include access_token value"
+            raise 'returned JSON data does not include access_token value'
 
     def get_host_url(self):
         try:
@@ -46,6 +46,6 @@ class ConfigHelper:
         return f'https://{host}/'
 
 
-if __name__ == "__main__":
-    config_helper = ConfigHelper("config.ini")
-    print(config_helper.get_access_token(), end="")
+if __name__ == '__main__':
+    config_helper = ConfigHelper('config.ini')
+    print(config_helper.get_access_token(), end='')
--- get_data.py
+++ get_data.py
@@ -6,29 +6,29 @@ import requests
 
 from config_helper import ConfigHelper
 
-DEFAULT_CONFIG_FILE = "config.ini"
+DEFAULT_CONFIG_FILE = 'config.ini'
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config_file", action="store",
-                        dest="config_file", default=DEFAULT_CONFIG_FILE,
-                        help=("the name of the config file to read from,"
-                              " ie `config.ini`"))
+    parser.add_argument('--config_file', action='store',
+                        dest='config_file', default=DEFAULT_CONFIG_FILE,
+                        help=('the name of the config file to read from,'
+                              ' ie `config.ini`'))
 
-    filters = parser.add_argument_group("filters")
-    filters.add_argument("--parking-zone", action="store",
-                         help=("search for buildings in"
-                               " parking zone PARKING_ZONE"))
-    filters.add_argument("--building-id", action="store",
-                         help="find the building with ID BUILDING_ID")
-    filters.add_argument("--campus", action="store",
-                         help="search for buildings located in campus CAMPUS")
-    filters.add_argument("--gi-restrooms", action="store_true",
-                         help=("search for building with"
-                               " gender inclusive restrooms"))
-    filters.add_argument("--query", action="store",
-                         help="search for locations using query QUERY")
+    filters = parser.add_argument_group('filters')
+    filters.add_argument('--parking-zone', action='store',
+                         help=('search for buildings in'
+                               ' parking zone PARKING_ZONE'))
+    filters.add_argument('--building-id', action='store',
+                         help='find the building with ID BUILDING_ID')
+    filters.add_argument('--campus', action='store',
+                         help='search for buildings located in campus CAMPUS')
+    filters.add_argument('--gi-restrooms', action='store_true',
+                         help=('search for building with'
+                               ' gender inclusive restrooms'))
+    filters.add_argument('--query', action='store',
+                         help='search for locations using query QUERY')
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -36,15 +36,15 @@ def parse_arguments():
     # query_filters are passed to the API,
     # filters are applied to values returned from the API
     return {
-        "config_file": args.config_file or DEFAULT_CONFIG_FILE,
-        "query_filters": {
-            "parkingZoneGroup": args.parking_zone,
-            "giRestroom": args.gi_restrooms,
-            "q": args.query,
-            "campus": args.campus,
+        'config_file': args.config_file or DEFAULT_CONFIG_FILE,
+        'query_filters': {
+            'parkingZoneGroup': args.parking_zone,
+            'giRestroom': args.gi_restrooms,
+            'q': args.query,
+            'campus': args.campus,
         },
-        "filters": {
-            "bldgID": args.building_id,
+        'filters': {
+            'bldgID': args.building_id,
         }
     }
 
@@ -58,25 +58,25 @@ def parse_query_filters(filters):
 
 
 def post_filter_locations(locations, args):
-    for filter_key in parse_query_filters(args["filters"]):
+    for filter_key in parse_query_filters(args['filters']):
         locations = [location for location in locations
                      if location['attributes'][filter_key] ==
-                     args["filters"][filter_key]]
-    if args["filters"]["bldgID"] is not None and len(locations) > 0:
+                     args['filters'][filter_key]]
+    if args['filters']['bldgID'] is not None and len(locations) > 0:
         return locations[0]
     return locations
 
 
 def fetch_locations(host, auth_token, args):
     headers = {
-        "Authorization": f"Bearer {auth_token}",
-        "Content-Type": "application/json"
+        'Authorization': f'Bearer {auth_token}',
+        'Content-Type': 'application/json'
     }
 
     params = {
-        "page[size]": 10000
+        'page[size]': 10000
     }
-    params.update(parse_query_filters(args["query_filters"]))
+    params.update(parse_query_filters(args['query_filters']))
 
     r = requests.get(f'{host}v1/locations',
                      params, headers=headers)
@@ -84,14 +84,14 @@ def fetch_locations(host, auth_token, args):
 
     data = r.json()
 
-    locations = data["data"]
+    locations = data['data']
 
     return post_filter_locations(locations, args)
 
 
 args = parse_arguments()
 
-cred_helper = ConfigHelper(args["config_file"])
+cred_helper = ConfigHelper(args['config_file'])
 
 host = cred_helper.get_host_url()
 auth_token = cred_helper.get_access_token()
